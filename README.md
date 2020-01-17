afac52727759c515cb1645cab225102df004c27a
jmbiancaData <james.m.bianca@gmail.com>
2020-01-16 18:55:40 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+##String Indexing
+Simple program using _"Hello World"_ to return _"Wool"_ to the console
---
+++ String_Indexing.py
@@ -0,0 +1,5 @@
+#James Bianca
+# String Indexing
+myString = "Hello World"
+# prints "Wool" to the console
+print(myString[6]+myString[7]+myString[4]+myString[3])
